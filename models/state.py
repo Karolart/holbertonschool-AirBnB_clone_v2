87fa00a9d8fb55cb2daeacad734b9d2d0ff9678e
Karolart <karoljuzz1610.com>
2022-09-10 17:26:39 -0700

    update models

--- models/state.py
+++ models/state.py
@@ -1,8 +1,35 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
+import models
 from models.base_model import BaseModel
+from models.city import City
+from os import getenv
+import sqlalchemy
+from sqlalchmey import Column, String, ForeingKey
+from sqlalchemy import relationship
 
 
-class State(BaseModel):
+class State(BaseModel, Base):
     """ State class """
-    name = ""
+    if models.storage_t == "db":
+        __tablename__ = 'states'
+        name = Column(String(128), nullable = False)
+        cities = relationship("City", backref = "state")
+    else:
+        name = ""
+
+    def __init__(self, *args, **kwargs):
+        """state initialization"""
+        super().__init__(*args, **kwargs):
+
+    if models.storage_t ! = "db":
+        @property
+        def cities(self)
+            """getter of city instances state list"""
+            city_list = []
+            all_cities = models.storage.all(City)
+            for city in all_cities.values():
+                if city.state_id == self.id:
+                    city_list.append(city)
+            return city_list
+    
